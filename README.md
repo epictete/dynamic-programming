0c85736f92e3a491eaaaedf9bcadeaa854713b15
John (jol) <jol@odoo.com>
2021-02-21 12:56:58 +0100

    Initial commit

---
+++ README.md
@@ -0,0 +1,10 @@
+# Dynamic Programming
+
+These exercices are aimed at practicing _dynamic programming_ using **memoization** and **tabulation**.
+
+It is written in JavaScript.
+
+#### Want to learn more ?
+
+All the concepts covered here are very well explained by Alvin Zablan from [Coderbyte](https://coderbyte.com/) in his course on [algorithms and data structures](https://coderbyte.com/starter-course/algorithms-and-data-structures).  
+You can find the original material [here](https://www.youtube.com/watch?v=oBt53YbR9Kk).
---
+++ count-construct.js
@@ -0,0 +1,38 @@
+const countConstruct = (target, wordBank, memo = {}) => {
+    if (target in memo) return memo[target];
+    if (target === "") return 1;
+
+    let totalCount = 0;
+
+    for (let word of wordBank) {
+        if (target.indexOf(word) === 0) {
+            const numWays = countConstruct(
+                target.slice(word.length),
+                wordBank,
+                memo
+            );
+            totalCount += numWays;
+        }
+    }
+
+    memo[target] = totalCount;
+    return totalCount;
+};
+
+console.log(countConstruct("purple", ["purp", "p", "ur", "le", "purpl"]));
+console.log(countConstruct("abcdef", ["ab", "abc", "cd", "def", "abcd"]));
+console.log(
+    countConstruct("skateboard", ["bo", "rd", "ate", "t", "ska", "sk", "boar"])
+);
+console.log(
+    countConstruct("eeeeeeeeeeeeeeeeeeeeeeeeeeef", [
+        "e",
+        "ee",
+        "eee",
+        "eeee",
+        "eeeee",
+        "eeeeee",
+        "eeeeeee",
+        "eeeeeeee",
+    ])
+);
